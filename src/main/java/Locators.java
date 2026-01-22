37f432181970841287163e8a5d3bb8a41f12fcf1
arupremraj@gmail.com <Shivani@2625>
2026-01-22 22:08:45 +0530

    23th Locators

--- src/main/java/Locators.java
+++ src/main/java/Locators.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Locators {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		WebDriver driver =new ChromeDriver();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
 		// wait for 3 sec on page if any element is not visible and its applicable for every element
@@ -18,7 +18,18 @@ public class Locators {
         driver.findElement(By.linkText("Forgot your password?")).click();
         
         driver.findElement(By.xpath("//input[@placeholder='Name']")).sendKeys("prem@gmail.com");
-        
+        Thread.sleep(2000);
+        driver.findElement(By.xpath("//input[@placeholder='Name']")).sendKeys("premraj");
+        driver.findElement(By.xpath("//input[@placeholder='Email']")).sendKeys("arupremraj@gmail.com");
+        driver.findElement(By.xpath("//input[@placeholder='Phone Number']")).sendKeys("8381054474");
+        driver.findElement(By.cssSelector(".reset-pwd-btn")).click();
+        System.out.println(driver.findElement(By.cssSelector("form p")).getText());
+        driver.findElement(By.className("go-to-login-btn")).click();
+        Thread.sleep(2000);
+        driver.findElement(By.cssSelector("input[placeholder*='User']")).sendKeys("PremrajA");
+        driver.findElement(By.cssSelector("input[placeholder*='Pass']")).sendKeys("rahulshettyacademy");
+        driver.findElement(By.xpath("//input[@value='rmbrUsername']")).click();
+        driver.findElement(By.className("submit")).click();
 	}
 
 }
